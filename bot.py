5f74608fb2538c6f7d9bf8d9145711cc05bbd337
sluicee <sluicee1@gmail.com>
2024-05-19 22:15:43 +0400

    echo favs emotes and phrases

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 
 import os
+import helpers
 from dotenv import load_dotenv
 from twitchio.ext import commands
 
@@ -13,6 +14,8 @@ if os.path.exists(dotenv_path):
 OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
 PREFIX = os.getenv('PREFIX')
 CHANNELS = os.getenv('CHANNELS').split(',')
+FAVS = ["widetime","Taah","cannyCat","1984",")))","well","dobro","sdd","PogT","ABOBA","TaaPls","WideCatGroove"]
+FAV_PHRASES = ["MyHonestReaction TeaTime","Bedge TeaTime","docnotL ботоводы блять"]
 class Bot(commands.Bot):
     
     def __init__(self):
@@ -35,13 +38,38 @@ class Bot(commands.Bot):
         })
 
         print(f'{message.author.name}: {message.content}')  # Вывод сообщений в консоль
+        
+        if "Taa" in message.content.split():
+            await self.send_message(message.channel.name, "Нет курению Taa")
+            
+        # Проверка наличия элементов и вывод первого совпавшего
+        for word in message.content.split():
+            if word in FAVS:
+                await self.send_message(message.channel.name, word)
+                print("Слово найдено в строке.")
+                break
+        else:
+            print("Ни один из элементов не найден")
+            
+        for phrase in FAV_PHRASES:
+            if phrase in message.content:
+                await self.send_message(message.channel.name, phrase)
+                print("Фраза найдена в строке.")
+                break
+        else:
+            print("Ни одна из фраз не найдена")
         await self.handle_commands(message)
         
     @commands.command(name='emotes')
     async def emotes(self, ctx, *, username: str = ''):
         await ctx.send(f'xdd')  # Отправка сообщения в чат
-
+        
+    @helpers.cooldown(2.5)
     async def send_message(self, channel, message):
-        await self.get_channel(channel).send(message)  # Отправка сообщения в чат
+        channel_obj = self.get_channel(channel)
+        if channel_obj:
+            await channel_obj.send(message)  # Отправка сообщения в чат
+        else:
+            print(f"Channel {channel} not found.")
 
 bot = Bot()
--- helpers.py
+++ helpers.py
@@ -2,6 +2,8 @@
 
 import os
 import requests
+import asyncio
+from functools import wraps
 
 API_URL_7TV = 'https://7tv.io/v3'
 USERS_7TV_API = '/users/'
@@ -36,3 +38,21 @@ def get_emotes(channel_id):
         return emotes
     else:
         print('No emotes found')
+
+
+
+def cooldown(seconds):
+    def decorator(func):
+        last_called = {}
+
+        @wraps(func)
+        async def wrapper(self, *args, **kwargs):
+            channel = args[0]  # предполагаем, что первый аргумент - канал
+            now = asyncio.get_event_loop().time()
+            if channel not in last_called or now - last_called[channel] >= seconds:
+                last_called[channel] = now
+                return await func(self, *args, **kwargs)
+            else:
+                print(f"Cooldown active for channel {channel}")
+        return wrapper
+    return decorator
